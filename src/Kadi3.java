d87c236827c222c0367cc08269fb06df22fc3ac4
unknown <x15g009@PC504N025.chiba-fjb.ac.jp>
2015-11-02 14:45:22 +0900

    課題３

---
+++ src/Kadi3.java
@@ -0,0 +1,12 @@
+
+public class Kadi3 {
+
+	public static void main(String[] args) {
+	System.out.print("船橋 太郎");System.out.print("船橋 太郎");
+	System.out.print("船橋 太郎");System.out.print("船橋 太郎");
+	System.out.print("船橋 太郎");System.out.print("船橋 太郎");
+	System.out.print("船橋 太郎");System.out.print("船橋 太郎");
+	System.out.print("船橋 太郎");System.out.print("船橋 太郎");
+	}
+
+}

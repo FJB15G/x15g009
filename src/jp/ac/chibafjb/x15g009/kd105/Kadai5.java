374cb042de065038232e5ad1f85b383599c91f82
unknown <x15g009@PC504N025.chiba-fjb.ac.jp>
2015-11-02 15:08:22 +0900

    課題５

---
+++ src/jp/ac/chibafjb/x15g009/kd105/Kadai5.java
@@ -0,0 +1,14 @@
+package jp.ac.chibafjb.x15g009.kd105;
+
+public class Kadai5 {
+
+	public static void main(String[] args) {
+		System.out.println("船橋太郎");
+		System.out.println("aaaaaaaa");
+		System.out.println("bbbbbbbb");
+		
+		
+
+	}
+
+}

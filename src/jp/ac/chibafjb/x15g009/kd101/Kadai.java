e604d3885e712f39345a4030510f03cc31401c3e
unknown <x15g009@PC504N025.chiba-fjb.ac.jp>
2015-11-02 14:25:51 +0900

    初期状態

---
+++ src/jp/ac/chibafjb/x15g009/kd101/Kadai.java
@@ -0,0 +1,11 @@
+package jp.ac.chibafjb.x15g009.kd101;
+
+public class Kadai {
+
+	public static void main(String[] args) {
+	System.out.println("船橋情報ビジネス専門学校");
+	System.out.println("047-425-1051");
+	System.out.println("Hello☆こんにちは");
+	}
+
+}

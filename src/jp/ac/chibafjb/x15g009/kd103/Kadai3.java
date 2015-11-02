21097fe5b48acc7e89a25a1850fbe6c632b3556e
unknown <x15g009@PC504N025.chiba-fjb.ac.jp>
2015-11-02 14:50:02 +0900

    課題３

---
+++ src/jp/ac/chibafjb/x15g009/kd103/Kadai3.java
@@ -0,0 +1,20 @@
+package jp.ac.chibafjb.x15g009.kd103;
+
+public class Kadai3 {
+
+	public static void main(String[] args) {
+		System.out.print("船橋 太郎");
+		System.out.print("船橋 太郎");
+		System.out.print("船橋 太郎");
+		System.out.print("船橋 太郎");
+		System.out.print("船橋 太郎");
+		System.out.print("船橋 太郎");
+		System.out.print("船橋 太郎");
+		System.out.print("船橋 太郎");
+		System.out.print("船橋 太郎");
+		System.out.print("船橋 太郎");
+
+
+	}
+
+}

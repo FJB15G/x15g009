c1e409795e6e752d8687705ef84c25df9d4dfbf3
unknown <x15g009@PC504N025.chiba-fjb.ac.jp>
2015-11-02 14:54:07 +0900

    課題４

---
+++ src/jp/ac/chibafjb/x15g009/kd104/Kadai4.java
@@ -0,0 +1,11 @@
+package jp.ac.chibafjb.x15g009.kd104;
+
+public class Kadai4 {
+
+	public static void main(String[] args) {
+		System.out.println("あいうえお　あいうえお　あいうえお");
+		System.out.println("あいうえお　あいうえお　あいうえお");
+		System.out.println("あいうえお　あいうえお　あいうえお");
+	}
+
+}

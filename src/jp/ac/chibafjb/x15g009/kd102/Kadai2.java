8b581c0be86119f58a24a93e41f8e87271cc9143
unknown <x15g009@PC504N025.chiba-fjb.ac.jp>
2015-11-02 14:40:26 +0900

    課題２

---
+++ src/jp/ac/chibafjb/x15g009/kd102/Kadai2.java
@@ -0,0 +1,18 @@
+package jp.ac.chibafjb.x15g009.kd102;
+
+public class Kadai2 {
+
+	public static void main(String[] args) {
+			System.out.println("船橋 太郎");
+			System.out.println("船橋 太郎");
+			System.out.println("船橋 太郎");
+			System.out.println("船橋 太郎");
+			System.out.println("船橋 太郎");
+			System.out.println("船橋 太郎");
+			System.out.println("船橋 太郎");
+			System.out.println("船橋 太郎");
+			System.out.println("船橋 太郎");
+			System.out.println("船橋 太郎");
+	}
+
+}

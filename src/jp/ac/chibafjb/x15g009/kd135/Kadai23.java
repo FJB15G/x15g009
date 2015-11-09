ae4532e0a618bdb250dc6085135907b1fff8bf84
unknown <x15g009@PC504N025.chiba-fjb.ac.jp>
2015-11-09 14:34:26 +0900

    課題２３

---
+++ src/jp/ac/chibafjb/x15g009/kd135/Kadai23.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g009.kd135;
+
+public class Kadai23 {
+
+	public static void main(String[] args) {
+		String str1 = "apple";
+		String str2 = "orange";
+		System.out.println("str1 = "+str1);
+		System.out.println("str2 = "+str2);
+
+	}
+
+}

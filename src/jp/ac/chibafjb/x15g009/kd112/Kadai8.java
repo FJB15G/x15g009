94edbdb14701478f86f4e294ea45c5223cc138bd
unknown <x15g009@PC504N025.chiba-fjb.ac.jp>
2015-11-09 13:55:08 +0900

    課題８

---
+++ src/jp/ac/chibafjb/x15g009/kd112/Kadai8.java
@@ -0,0 +1,11 @@
+package jp.ac.chibafjb.x15g009.kd112;
+
+public class Kadai8 {
+
+	public static void main(String[] args) {
+		for(int i = 0;i<=99;i++)
+			System.out.println("船橋情報ビジネス専門学校");
+
+	}
+
+}

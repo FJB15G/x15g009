9493c110099930c5daaed3f886fb90a3fbbeae65
unknown <x15g009@PC504N025.chiba-fjb.ac.jp>
2015-11-09 14:06:00 +0900

    課題１２

---
+++ src/jp/ac/chibafjb/x15g009/kd116/Kadai12.java
@@ -0,0 +1,11 @@
+package jp.ac.chibafjb.x15g009.kd116;
+
+public class Kadai12 {
+
+	public static void main(String[] args) {
+		for(int i = 0;i<=1;i++)
+			System.out.println("あいうえお　あいうえお　あいうえお\nかきくけこ　かきくけこ　かきくけこ\nさしすせそ　さしすせそ　さしすせそ");
+
+	}
+
+}

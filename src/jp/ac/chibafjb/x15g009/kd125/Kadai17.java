a93add8ebe44c59ad583b8fc3fda150530931775
unknown <x15g009@PC504N025.chiba-fjb.ac.jp>
2015-11-09 14:15:31 +0900

    課題１７

---
+++ src/jp/ac/chibafjb/x15g009/kd125/Kadai17.java
@@ -0,0 +1,11 @@
+package jp.ac.chibafjb.x15g009.kd125;
+
+public class Kadai17 {
+
+	public static void main(String[] args) {
+		for(int i = 10;i<=30;i=i+10)
+			System.out.println(i+"＊");
+
+	}
+
+}

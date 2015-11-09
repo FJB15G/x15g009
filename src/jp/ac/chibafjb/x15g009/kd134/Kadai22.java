d63bed9b5d17ed9ea138cc62c70df5876bb56aea
unknown <x15g009@PC504N025.chiba-fjb.ac.jp>
2015-11-09 14:31:25 +0900

    課題２２

---
+++ src/jp/ac/chibafjb/x15g009/kd134/Kadai22.java
@@ -0,0 +1,14 @@
+package jp.ac.chibafjb.x15g009.kd134;
+
+public class Kadai22 {
+
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 20;
+		y = x;
+		System.out.println("x = "+x);
+		System.out.println("y = "+y);
+
+	}
+
+}

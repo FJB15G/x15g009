90f5527f72dc30ca8abb12ef04f8f3e936e839cb
unknown <x15g009@PC504N025.chiba-fjb.ac.jp>
2015-11-09 14:28:59 +0900

    課題２１

---
+++ src/jp/ac/chibafjb/x15g009/kd133/Kadai21.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g009.kd133;
+
+public class Kadai21 {
+
+	public static void main(String[] args) {
+		int x = 5;
+		int y = 10;
+		System.out.print("x+y = "+(x+y));
+
+	}
+
+}

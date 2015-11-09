442ead5ddbf4081d8011ecf883f302d5626998ec
unknown <x15g009@PC504N025.chiba-fjb.ac.jp>
2015-11-09 14:45:35 +0900

    課題２６

---
+++ src/jp/ac/chibafjb/x15g009/kd141/Kadai26.java
@@ -0,0 +1,17 @@
+package jp.ac.chibafjb.x15g009.kd141;
+
+import java.util.Scanner;
+
+public class Kadai26 {
+
+	public static void main(String[] args) {
+		System.out.print("整数を1つ入力してください");
+		Scanner sin = new Scanner(System.in);
+		int num = sin.nextInt();
+		sin.close();
+		System.out.println("あなたが入力した数は"+num+"です");
+		
+
+	}
+
+}

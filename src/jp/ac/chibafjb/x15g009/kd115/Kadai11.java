d3f96f090a0f204c10abb05d5a9a97cde9409e04
unknown <x15g009@PC504N025.chiba-fjb.ac.jp>
2015-11-09 14:03:20 +0900

    課題１１

---
+++ src/jp/ac/chibafjb/x15g009/kd115/Kadai11.java
@@ -0,0 +1,15 @@
+package jp.ac.chibafjb.x15g009.kd115;
+
+public class Kadai11 {
+
+	public static void main(String[] args) {
+		for(int a = 0;a<=1;a++)
+			System.out.println("あいうえお　あいうえお　あいうえお");
+		for(int a = 0;a<=1;a++)
+			System.out.println("かきくけこ　かきくけこ　かきくけこ");
+		for(int a = 0;a<=1;a++)
+			System.out.println("さしすせそ　さしすせそ　さしすせそ");
+
+	}
+
+}
